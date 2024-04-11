5894402ec4968c8724b983ccdd06c7e6399caf27
Pravesh1800 <166682702+Pravesh1800@users.noreply.github.com>
2024-04-11 13:55:50 +0530

    Update 1_🏠_Home.py

--- titanic/1_🏠_Home.py
+++ titanic/1_🏠_Home.py
@@ -6,6 +6,8 @@ import time
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+st.set_option('theme', 'light')
+
 st.set_page_config(
     page_title="Titanic Survival Prediction",
     page_icon="📈",
